d601b8967f7c5223b6172c3637dd3413efd09d35
shootkiran <shootkiran@gmail.com>
2021-05-07 08:44:53 +0545

    Updateting to Laravel 8

--- resources/views/venue/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <h1>{{$venue->name}}</h1>
-    <div class="row">
-      Under Construction. Please Contact us for more info.
-    </div>
-@endsection
